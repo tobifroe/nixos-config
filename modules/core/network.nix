9225148be96e4c73d281e330ec1bebd2c8f5ca7a
tobifroe <froeltob@pm.me>
2024-11-11 12:00:08 +0100

    chore: updates, adapt hyprland config

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -3,7 +3,7 @@
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    nameservers = [ "9.9.9.9" "149.112.112.112" "1.1.1.1" ];
+    nameservers = [ "192.168.1.172" "9.9.9.9" "149.112.112.112" "1.1.1.1" ];
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 80 443 59010 59011 ];
--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -95,14 +95,6 @@
           new_optimizations = true;
           xray = true;
         };
-
-        drop_shadow = true;
-
-        shadow_ignore_window = true;
-        shadow_offset = "0 2";
-        shadow_range = 20;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(00000055)";
       };
 
       animations = {
